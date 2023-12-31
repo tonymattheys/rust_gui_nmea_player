087ceea69af6b812555012a0498724357a413183
Tony Mattheys <tony@mattheys.com>
2023-12-31 09:11:03 -0700

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
+struct Cli {
+    #[arg(short, long)]
+    two: Option<String>,
+    #[arg(short, long, value_name = "FILE")]
+    one: PathBuf,
+}
+
 // This declaration will look for a file named `my.rs` and will
 // insert its contents inside a module named `my` under this scope
 mod my;
@@ -7,6 +19,15 @@ fn function() {
 }
 
 fn main() {
+    let cli = Cli::parse();
+
+    if let Some(two) = cli.two.as_deref() {
+        println!("Value for two: {two}");
+    }
+
+    println!("two: {:?}", cli.two);
+    println!("one: {:?}", cli.one);
+
     my::function();
 
     function();
@@ -14,5 +35,4 @@ fn main() {
     my::indirect_access();
 
     my::nested::function();
-    
 }
